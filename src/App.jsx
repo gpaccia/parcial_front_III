a0699cc9d4c256add7e9dcc34b47ddfd4a66bae9
Gaston Pacciaroni <gaston.pacciaroni@tecnotree.com>
2023-05-29 22:29:49 -0300

    clase 10 pre parcial con correcciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import Card from "./Card";
+import Card from "./Components/Card"
 
 function App() {
   return (
     <div className="App">
       <h1>Carga de estudiantes</h1>
       <form></form>
-      <Card />
+      <Card/>
     </div>
   );
 }
